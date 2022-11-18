7a5e7087c1b1eb2aea5853c782f8c4c790d289ca
xuzhen <xuzhen@hfut.com>
2022-11-18 10:36:30 +0800

    master commit2

--- src/test/java/com/xz/Mybatisplus01QuickstartApplicationTests.java
+++ src/test/java/com/xz/Mybatisplus01QuickstartApplicationTests.java
@@ -84,6 +84,7 @@ class Mybatisplus01QuickstartApplicationTests {
         System.out.println("hello git!");
         System.out.println("hello git2!");
         System.out.println("hello git3!");
+        System.out.println("hello git4!");
     }
 
 }
